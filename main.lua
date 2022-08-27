f4c9078f0b9a5f5d107cfa76edef2d850afbc0cb
jacobbeatty <54652544+jacobbeatty@users.noreply.github.com>
2022-08-27 14:18:36 +1000

    added UI and player

--- main.lua
+++ main.lua
@@ -1,5 +1,71 @@
-_G.love = require("love")
+love = require("love")
+
+function love.load()
+    love.window.setTitle("Game")
+    love.graphics.setBackgroundColor(255, 255, 255)
+
+    windowHeight = love.graphics.getHeight()
+    windowWidth = love.graphics.getWidth()
+
+    centerX = windowWidth/2
+	centerY = windowHeight/2
+
+
+    time_remaining = 15
+    score = 5
+    level = 1
+end
+
+function love.update(dt)
+    --round to two decimal places
+    time_remaining = math.floor((time_remaining - dt) * 100) / 100
+
+    if time_remaining < 0 then
+        time_remaining = 0
+    end
+
+    --close the game if the player presses escape
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
+
+    --rotate the outer pointer around the center following the mouse
+    angle = math.atan2(love.mouse.getY() - windowHeight / 2, love.mouse.getX() - windowWidth / 2)
+end
 
 function love.draw()
-    love.graphics.print("This is my message now!", 400, 400)
+    
+    --draw board
+    love.graphics.setLineWidth(15)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("line", 25, 25, 750, 550)
+
+    --draw timer
+    love.graphics.print(time_remaining, 40, 40)
+
+    --draw score
+    love.graphics.print(score, windowWidth-50, 40)
+
+    --draw level name
+    love.graphics.print("Level: "..level, windowWidth/2, 40)
+
+    --draw player
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle("fill", 400, 300, 15)
+    --draw outer circle
+    love.graphics.setLineWidth(5)
+    love.graphics.circle("line", 400, 300, 30)
+
+    --rotate around the center
+    love.graphics.translate(centerX, centerY)
+	love.graphics.rotate(angle)
+	love.graphics.translate(-centerX, -centerY)
+    --draw outer pointer
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.line(400, 300, 400, 270)
+    love.graphics.rotate(angle)
+    
+
+
+
 end
